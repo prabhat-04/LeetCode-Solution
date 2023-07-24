d47adffbf754155c843644972375fd011e27fb82
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2023-07-24 12:42:43 +0530

    Time: 5 ms (76.33%), Space: 7.9 MB (64.11%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        if(n==0)
+            return {0};
+        if(n==1)
+            return {0,1};
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0)
+                dp[i]=dp[i/2];
+            else
+                dp[i]=dp[i/2]+1;
+        }
+        return dp;
+    }
+};
