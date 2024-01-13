81b23a6ca6236a157e601d5590965202eb32eafc
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-01-13 19:14:00 +0530

    Time: 67 ms (52.02%) | Memory: 17.2 MB (19.43%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
+        ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
         unordered_map<char,int> mp;
         for(int i=0;i<s.size();i++){
             mp[s[i]]++;
