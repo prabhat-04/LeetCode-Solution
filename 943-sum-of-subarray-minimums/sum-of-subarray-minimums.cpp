d4dae6c9f4f12d4a2b4dee794cda2372caa83f0d
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-01-20 18:40:39 +0530

    Time: 36 ms (100.00%) | Memory: 44.7 MB (14.86%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int mod = 1e9+7;
-    int sumSubarrayMins(vector<int>& arr){
+    int mod = 1e9 + 7;
+    int sumSubarrayMins(vector<int>& arr) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n = arr.size();
         vector<long> dp(n);
         stack<int> st;
-        long sum=0;
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && arr[i]< arr[st.top()])
+        long sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[i] < arr[st.top()])
                 st.pop();
-            if(st.empty())
-                dp[i] = ((arr[i]%mod) * ((n-i)%mod))%mod;
+            if (st.empty())
+                dp[i] = ((arr[i] % mod) * ((n - i) % mod)) % mod;
             else
-                dp[i] = ((arr[i]*(st.top()-i))%mod + (dp[st.top()])%mod)%mod;
+                dp[i] =
+                    ((arr[i] * (st.top() - i)) % mod + (dp[st.top()]) % mod) %
+                    mod;
             st.push(i);
-            sum = (sum%mod + dp[i]%mod)%mod;
+            sum = (sum % mod + dp[i] % mod) % mod;
         }
         return sum;
     }
