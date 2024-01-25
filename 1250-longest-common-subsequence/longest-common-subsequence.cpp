4bdd73abb955c6b90fa97324615be2d120c4fcc9
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-01-25 10:53:01 +0530

    Time: 13 ms (98.70%) | Memory: 11.9 MB (71.87%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -6,10 +6,11 @@ public:
         cout.tie(NULL);
         int n=text1.size(),m=text2.size();
         int dp[n+1][m+1];
-        for(int i=0;i<=n;i++)
-            dp[i][0]=0;
-            for(int j=1;j<=m;j++)
-                dp[0][j]=0;
+        memset(dp,0,sizeof(dp));
+        // for(int i=0;i<=n;i++)
+        //     dp[i][0]=0;
+        //     for(int j=1;j<=m;j++)
+        //         dp[0][j]=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(text1[i-1]==text2[j-1])
