ebb818e643175b2223206ecaf5d208a69e3a2091
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-05 21:10:09 +0530

    Time: 27 ms (72.30%) | Memory: 13.2 MB (10.77%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         unordered_map<char,int> temp;
         for(auto ch:s){
             temp[ch]++;
