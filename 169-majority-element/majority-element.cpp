76867640df170a216f050b9b5f50e3930c7c536e
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-12 12:27:13 +0530

    Time: 17 ms (35.13%) | Memory: 23.4 MB (6.31%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        unordered_map<int,int> mp;
+        for(int x:nums){
+            mp[x]++;
+            if(mp[x]>nums.size()/2)
+                return x;
+        }
+        return -1;
+    }
+};
