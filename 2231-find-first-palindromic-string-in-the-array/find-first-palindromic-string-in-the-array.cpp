5284ac7a3a3206af7379bcfe2bad6f7ccae87af1
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-13 17:39:02 +0530

    Time: 30 ms (98.78%) | Memory: 26.7 MB (24.97%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool palindrome(string &word){
+        int i=0,j=word.size()-1;
+        while(i<j){
+            if(word[i]!=word[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    string firstPalindrome(vector<string>& words) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        for(auto word:words){
+            if(palindrome(word))
+                return word;
+        }
+        return "";
+    }
+};
