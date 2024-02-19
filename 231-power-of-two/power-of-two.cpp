8b5dfe452c80b612d7416d8375aeeb2e639a94a2
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-19 22:52:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (19.05%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
+        // ios::sync_with_stdio(false);
+        // cin.tie(nullptr);
          return n && !(n & (n-1l));
     }
 };
