a26df5c726933740ef30997ff83fde51f413788a
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-23 14:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (7.75%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.cpp
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -5,22 +5,14 @@ public:
         unordered_map<char,int> mp;
         for(int i=0;i<order.size();i++)
             mp[order[i]]=i;
-        vector<pair<char,int>> vec;
-        int k= mp.size();
-        for(int i=0;i<s.size();i++){
-            if(mp.find(s[i])!=mp.end())
-                vec.push_back({s[i],mp[s[i]]});
-            else{
-                vec.push_back({s[i],k});
-                k++;
-            }
-        }
-        sort(vec.begin(),vec.end(),[](pair<char,int> &a,pair<char,int> &b){
-            return a.second<b.second;
-            });
-        s="";
-        for(auto x:vec)
-            s+=x.first;
+
+        sort(s.begin(),s.end(),[&](char &a,char &b){
+            if(mp.find(a)!=mp.end() && mp.find(b)!=mp.end())
+                return mp[a] < mp[b];
+            if(mp.find(a)==mp.end())
+                return false;
+            else return true;
+        });
         return s;
     }
 };
