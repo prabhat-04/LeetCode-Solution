db4d895397f66a4ebc3b41542eea45ad19008900
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-27 10:41:41 +0530

    Time: 64 ms (79.14%) | Memory: 27.3 MB (56.45%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<nums.size()-2;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
             int j=i+1,k=nums.size()-1;
             while(j<k){
