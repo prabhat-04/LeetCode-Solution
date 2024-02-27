4d6760191ebd34a716ba8710e7a1eda8cdf04001
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-27 11:09:51 +0530

    Time: 11 ms (94.15%) | Memory: 12.8 MB (34.96%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -5,6 +5,7 @@ public:
         sort(nums.begin(),nums.end());
 
         for(int i=0;i<nums.size()-2;i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
             int j=i+1,k=nums.size()-1;
             int tar = target - nums[i];
             while(j<k){
