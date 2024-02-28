e7f6400bd4acce5251a039bd435b322338e51702
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-28 11:45:48 +0530

    Time: 6 ms (39.42%) | Memory: 10 MB (46.39%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.cpp
+++ 1983-maximum-population-year/maximum-population-year.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        vector<int> popu(102,0);
+        vector<int> popu(110,0);
         for(auto x:logs){
             popu[x[0]-1950]++;
             popu[x[1]-1950]--;
         }
-        int maxPopu=popu[0], ans = -1;
-        for(int i=1;i<102;i++){
+        int maxPopu=popu[0], ans = 0;
+        for(int i=1;i<110;i++){
             popu[i] = popu[i] + popu[i-1];
             if(maxPopu < popu[i]){
                 maxPopu=popu[i];
                 ans= i;
             }
         }
-        return ans+1950==1949?1950:ans+1950;
+        return ans+1950;
     }
 };
