7e120d8a8f7f1e975095f39fdad8c05966b47903
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-02-28 16:10:06 +0530

    Time: 25 ms (50.20%) | Memory: 26.8 MB (24.77%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -4,25 +4,25 @@ public:
         int n =  nums.size();
         vector<int> ans(n);
         stack<int> st;
-        vector<bool> vis(n,false);
+        vector<int> vec;
         for(int i=n-1;i>=0;i--){
             while(!st.empty() && st.top()<=nums[i])
                 st.pop();
             if(st.empty()){  
                 ans[i]=-1;
-                vis[i]=true;
+                vec.push_back(i);
             }
             else    ans[i] = st.top();
             st.push(nums[i]);
         }
 
-        for(int i=n-1;i>=0;i--){
-            if(vis[i]){
-                while(!st.empty() && st.top()<=nums[i])
-                    st.pop();
-                if(st.empty())  ans[i]=-1;
-                else    ans[i] = st.top();
+        for(int i=0;i<vec.size();i++){
+            while(!st.empty() && st.top()<=nums[vec[i]])
+                st.pop();
+            if(st.empty()){  
+                ans[vec[i]]=-1;
             }
+            else    ans[vec[i]] = st.top();
         }
         return ans;
     }
