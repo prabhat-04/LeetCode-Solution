41064f189f66bb84573aac7d24bf04b8f7e738b0
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-03-11 15:01:26 +0530

    Time: 108 ms (26.36%) | Memory: 16.6 MB (79.81%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL) return list2;
+        if(list2==NULL) return list1;
+
+        ListNode* ptr,*head ;
+        if(list1->val <=list2->val){
+            ptr=list1;
+            head=list1;
+            list1=list1->next;
+        }
+        else{
+            ptr=list2;
+            head=list2;
+            list2=list2->next;
+        }
+
+        while(list1 && list2){
+            if(list1->val <=list2->val){
+                ptr->next=list1;
+                list1=list1->next;
+            }
+            else{
+                ptr->next=list2;
+                list2=list2->next;
+            }
+            ptr=ptr->next;
+        }
+        if(list1)
+            ptr->next=list1;
+        if(list2)
+            ptr->next=list2;
+        return head;
+
+    }
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        ListNode *ans;
+        while(lists.size()>1){
+            ans = mergeTwoLists(lists[lists.size()-1],lists[lists.size()-2]);
+            lists.pop_back();
+            lists.pop_back();
+            lists.push_back(ans);
+        }
+        return lists[0];
+    }
+};
