aa772c1f33db725ddc514433206b2c1443f7c9bd
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-03-14 13:57:28 +0530

    Time: 171 ms (14.49%) | Memory: 17.1 MB (35.33%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -12,35 +12,43 @@ class Solution {
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1==NULL) return list2;
         if(list2==NULL) return list1;
-
-        ListNode* ptr,*head ;
-        if(list1->val <=list2->val){
-            ptr=list1;
-            head=list1;
-            list1=list1->next;
+        if(list1->val<=list2->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
         }
         else{
-            ptr=list2;
-            head=list2;
-            list2=list2->next;
+            list2->next = mergeTwoLists(list1,list2->next);
+            return list2;
         }
 
-        while(list1 && list2){
-            if(list1->val <=list2->val){
-                ptr->next=list1;
-                list1=list1->next;
-            }
-            else{
-                ptr->next=list2;
-                list2=list2->next;
-            }
-            ptr=ptr->next;
-        }
-        if(list1)
-            ptr->next=list1;
-        if(list2)
-            ptr->next=list2;
-        return head;
+        // ListNode* ptr,*head ;
+        // if(list1->val <=list2->val){
+        //     ptr=list1;
+        //     head=list1;
+        //     list1=list1->next;
+        // }
+        // else{
+        //     ptr=list2;
+        //     head=list2;
+        //     list2=list2->next;
+        // }
+
+        // while(list1 && list2){
+        //     if(list1->val <=list2->val){
+        //         ptr->next=list1;
+        //         list1=list1->next;
+        //     }
+        //     else{
+        //         ptr->next=list2;
+        //         list2=list2->next;
+        //     }
+        //     ptr=ptr->next;
+        // }
+        // if(list1)
+        //     ptr->next=list1;
+        // if(list2)
+        //     ptr->next=list2;
+        // return head;
 
     }
 public:
