df0aed920b2407d24c7d30e5c4d04bda9c3e884c
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-03-14 15:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (34.43%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -18,27 +18,46 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
         if(head==NULL) return NULL;
-        Node *ptr=head;
+        // Node *ptr=head;
+        // while(ptr!=NULL){
+        //     Node *newNode = new Node(ptr->val);
+        //     newNode->next = ptr->next;
+        //     ptr->next = newNode;
+        //     ptr=ptr->next->next;
+        // }
+        // ptr=head;
+        // while(ptr!=NULL){
+        //     ptr->next->random = ptr->random?ptr->random->next:NULL;
+        //     ptr=ptr->next->next;
+        // }
+        // Node *ans = head->next,*temp=head->next;
+        // ptr=head;
+        // while(ptr && temp){
+        //     ptr->next = ptr->next?ptr->next->next:NULL;
+        //     temp->next = temp->next?temp->next->next:NULL;
+        //     ptr=ptr->next;
+        //     temp=temp->next;
+        // }
+        // return ans;
+        unordered_map<Node*,Node*> mp;
+        Node *ans = new Node(head->val);
+        mp[head] = ans;
+        Node *ptr=head->next,*curr=ans;
         while(ptr!=NULL){
-            Node *newNode = new Node(ptr->val);
-            newNode->next = ptr->next;
-            ptr->next = newNode;
-            ptr=ptr->next->next;
-        }
-        ptr=head;
-        while(ptr!=NULL){
-            ptr->next->random = ptr->random?ptr->random->next:NULL;
-            ptr=ptr->next->next;
-        }
-        Node *ans = head->next,*temp=head->next;
-        ptr=head;
-        while(ptr && temp){
-            ptr->next = ptr->next?ptr->next->next:NULL;
-            temp->next = temp->next?temp->next->next:NULL;
+            Node * newNode = new Node(ptr->val);
+            mp[ptr] = newNode;
+            curr->next = newNode;
             ptr=ptr->next;
-            temp=temp->next;
+            curr = curr->next;
+        }
+
+        ptr=head;
+        while(ptr!=NULL){
+            if(ptr->random){
+                mp[ptr]->random = mp[ptr->random];
+            }
+            ptr=ptr->next;
         }
         return ans;
-        
     }
 };
