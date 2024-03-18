8366cab4c8d3317585c3c6b432f5b0926c56c95a
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-03-18 15:33:11 +0530

    Time: 142 ms (98.70%) | Memory: 144.5 MB (92.90%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -9,6 +9,14 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ int speed = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
 class Solution {
     int help(TreeNode *root){
         if(!root) return INT_MAX;
