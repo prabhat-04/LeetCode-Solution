ecfa685600062f84a1696c5787c13c93fd3fb878
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-03-18 14:33:59 +0530

    Time: 3 ms (70.96%) | Memory: 17.3 MB (36.87%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -13,8 +13,7 @@ class Solution {
     bool isMirror(TreeNode *root1,TreeNode *root2){
         if(!root1 && !root2) return true;
         if(!root1 || !root2) return false;
-        if(root1->val != root2->val) return false;
-        return isMirror(root1->left,root2->right) && isMirror(root1->right,root2->left);
+        return (root1->val == root2->val) && isMirror(root1->left,root2->right) && isMirror(root1->right,root2->left);
     }
 public:
     bool isSymmetric(TreeNode* root) {
