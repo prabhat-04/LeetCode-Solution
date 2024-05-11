a58cca66661e6b3eaeca2fa9072ee18174b29552
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-05-11 17:26:49 +0530

    Time: 25 ms (69.55%) | Memory: 26.5 MB (51.68%) - LeetSync

---
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+        int n = quality.size();
+        double ans = numeric_limits<double>::max();
+        double curr = 0;
+        vector<pair<double,int>> vec;
+        for(int i=0;i<n;i++){
+            vec.push_back({static_cast<double>(wage[i])/quality[i],quality[i]});
+        }
+        sort(vec.begin(),vec.end());
+        priority_queue<int> pq;
+        for(int i=0;i<n;i++){
+            pq.push(vec[i].second);
+            curr+=vec[i].second;
+
+            if(pq.size()>k){
+                curr-=pq.top();
+                pq.pop();
+            }
+            if(pq.size()==k){
+                ans = min(ans,curr * vec[i].first);
+            }
+        }
+        return ans;
+    }
+};
