8b3c6255d8d8fd16b35987f3b79accadea20a183
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-05-28 21:47:35 +0530

    Time: 3 ms (97.07%) | Memory: 9.5 MB (12.01%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        vector<int> dp(s.size());
+        for(int i=0;i<s.size();i++){
+            dp[i]=abs(s[i]-t[i]);
+        }
+        int i=0,j=0,ans = INT_MIN,curr=0;
+        while(j<s.size()){
+            curr+=dp[j];
+            while(i<=j && curr>maxCost){
+                curr-=dp[i];
+                i++;
+            }
+            j++;
+            ans = max(ans,j-i);
+        }
+        return ans;
+    }
+};
