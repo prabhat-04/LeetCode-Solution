ab37b70140268e22225237ddc07cca22d44bb21d
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-06-19 17:01:42 +0530

    Time: 76 ms (97.56%) | Memory: 69.1 MB (28.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int l = INT_MAX,h=INT_MIN;
-        for(auto it:bloomDay){
-            l = min(l,it);
-            h = max(h,it);
-        }
-        int minDays = INT_MAX;
-        while(l<=h){
-            int mid = l + (h-l)/2;
-            int cnt=0,ans=0;
-            for(int i=0;i<bloomDay.size();i++){
-                if(bloomDay[i]<=mid){
-                    cnt++;
-                }
-                else
-                    cnt=0;
-                if(cnt>=k){
-                    ans++;
-                    cnt=0;
-                }
-            }
-            if(ans>=m){
-                minDays = mid;
-                h=mid-1;
-            }
-            else
-                l=mid+1;
-            // cout<<l<<" "<<h<<endl;
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        
 
+        int left = 0;
+        int right = *max_element(bloomDay.begin(), bloomDay.end());
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int adjacentFlowers = 0;
+            int bouquets = 0;
+
+            for (int i = 0; i < bloomDay.size(); ++i) {
+                if (bloomDay[i] <= mid) {
+                    adjacentFlowers++;
+                } else {
+                    adjacentFlowers = 0;
+                }
+
+                if (adjacentFlowers >= k) {
+                    bouquets++;
+                    adjacentFlowers = 0;
+                }
+            }
+
+            if (bouquets >= m) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
         }
-        return minDays==INT_MAX?-1:minDays;
+
+        return result;
     }
 };
