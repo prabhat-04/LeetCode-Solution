25f28da78e0609dc21821942e7c7acd8e76a2558
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-07-19 00:19:03 +0530

    Time: 15 ms (65.25%) | Memory: 27.2 MB (22.50%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -9,43 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
-    void postorder(TreeNode *root,vector<TreeNode*> &ans,unordered_set<int> &s,TreeNode* par, bool isLeft ){
-        if(!root) return;
-        postorder(root->left,ans,s,root,true);
-        postorder(root->right,ans,s,root,false);
-        if(s.find(root->val)!=s.end()){
-            if(root->left)
-                ans.push_back(root->left);
-            if(root->right)
-                ans.push_back(root->right);
-            if(par){
-                if(isLeft)
-                    par->left = NULL;
-                else
-                    par->right = NULL;
+    void postorder(TreeNode* node, vector<TreeNode*>& remainingTrees, unordered_set<int>& toDeleteSet, TreeNode* parent, bool isLeftChild) {
+        if (!node) return;
+        postorder(node->left, remainingTrees, toDeleteSet, node, true);
+        postorder(node->right, remainingTrees, toDeleteSet, node, false);
+        if (toDeleteSet.find(node->val) != toDeleteSet.end()) {
+            if (node->left) remainingTrees.push_back(node->left);
+            if (node->right) remainingTrees.push_back(node->right);
+            if (parent) {
+                if (isLeftChild) parent->left = nullptr;
+                else parent->right = nullptr;
             }
         }
     }
+
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        unordered_set<int> s;
-        for(auto it:to_delete){
-            s.insert(it);
-        }
-        vector<TreeNode*>ans;
-        if(s.find(root->val)==s.end()){
-            ans.push_back(root);
-           
-        }
-         postorder(root,ans,s,NULL,true);
-        // else{
-        //     ans.push_back(root->left);
-        //     ans.push_back(root->right);
-        //     postorder(root->left,ans,s,root,true);
-        //     postorder(root->right,ans,s,root,false);
-        // }
-        return ans;
+        unordered_set<int> toDeleteSet(to_delete.begin(), to_delete.end());
+        vector<TreeNode*> remainingTrees;
 
+        // If the root is not to be deleted, add it to the result
+        if (toDeleteSet.find(root->val) == toDeleteSet.end()) {
+            remainingTrees.push_back(root);
+        }
+
+        // Perform postorder traversal to delete nodes
+        postorder(root, remainingTrees, toDeleteSet, nullptr, true);
+
+        return remainingTrees;
     }
 };
