ab731a3e170ef3d6af728792062df6d68bf56eee
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-07-30 14:06:00 +0530

    Time: 99 ms (46.89%) | Memory: 24.7 MB (51.83%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         stack<char>st;
+        int n = s.size();
         int ans =0;
-        for(int i=0;i<s.size();i++){
+        for(int i=0;i<n;i++){
             if(!st.empty() && s[i]=='a' && st.top()=='b'){
                 st.pop();
                 ans++;
