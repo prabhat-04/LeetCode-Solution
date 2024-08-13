059b5126742a62c80631dbc6dcfe15a15f816426
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-08-13 20:54:55 +0530

    Time: 20 ms (11.00%) | Memory: 19.1 MB (9.43%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
-public:
-    vector<vector<int>> result;
-    
-    void comsum(vector<int> &curr, int target, int sum, vector<int> &candidates, int curInd, int n){
-        if(target == sum){
+    void solve(vector<int> &arr,int target,int sum,int idx,vector<int> curr,vector<vector<int>> &result){
+        int n = arr.size();
+        if(sum==target){
             result.push_back(curr);
             return;
         }
-        else if(sum>target){
-            return;
-        }
+        if(idx>=n || sum>target) return;
         
-        for(int i = curInd; i < n; i++){
-            if(i != curInd && candidates[i]==candidates[i-1])               //to avoid picking up the same combnations i.e. we don't pick same element for certain kth position of a combination 
-                continue;
-            sum += candidates[i];
-            curr.push_back(candidates[i]);
-            comsum(curr, target, sum, candidates, i+1, n);
-            sum -= candidates[i];
+        for(int i=idx;i<n;i++){
+            if(i !=idx && arr[i]==arr[i-1]) 
+                continue;   
+            sum+=arr[i];
+            curr.push_back(arr[i]);
+            solve(arr,target,sum,i+1,curr,result);
             curr.pop_back();
+            sum-=arr[i];
         }
         
     }
+public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
         vector<int> curr;
-        int n = candidates.size();
-        sort(candidates.begin(), candidates.end());
-        comsum(curr, target, 0, candidates, 0, n);
+        sort(candidates.begin(),candidates.end());
+        solve(candidates,target,0,0,curr,result);
         return result;
     }
 };
