43c209d4d1ca49042e8af98d476e7ed3a75e2472
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-09-18 16:58:49 +0530

    Time: 10 ms (29.14%) | Memory: 17.5 MB (14.46%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -1,11 +1,8 @@
 class Solution {
-public:
-    static bool mycmp(const string &a,const string &b){
-        string l = a+b;
-        string r = b+a;
-        return l>r;
+    static bool cmp(string &a,string &b){
+       return (a+b) >(b+a);
     }
-
+public:
     string largestNumber(vector<int>& nums) {
         vector<string> vec;
         int zero=0;
@@ -14,8 +11,8 @@ public:
             if(nums[i]==0) zero++;
         }
         if(zero==nums.size()) return "0";
-        sort(vec.begin(),vec.end(),mycmp);
-        string ans="";
+        sort(vec.begin(),vec.end(),cmp);
+        string ans= "";
         for(int i=0;i<vec.size();i++)
             ans+=vec[i];
         return ans;
