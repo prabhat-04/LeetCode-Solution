aceadbebf3f98d459a2b95e459a961da65478e7e
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-09-27 10:09:50 +0530

    Time: 75 ms (87.49%) | Memory: 37.9 MB (90.93%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.cpp
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -1,35 +1,23 @@
 class MyCalendarTwo {
-    int maxOverlappedBooking ;
-    map<int,int> bookingCount;
+    vector<pair<int,int>> b;
+    vector<pair<int,int>>db;
 public:
     MyCalendarTwo() {
-        maxOverlappedBooking = 2;
+        
     }
     
     bool book(int start, int end) {
-        bookingCount[start]++;
-        bookingCount[end]--;
-        int overlappedBooking =0;
-        for(auto &bookings:bookingCount){
-            overlappedBooking+=bookings.second;
-            if(overlappedBooking > maxOverlappedBooking){
-                bookingCount[start]--;
-                bookingCount[end]++;
-                if(bookingCount[start]==0){
-                    bookingCount.erase(start);
-                }
-                if(bookingCount[end]==0){
-                    bookingCount.erase(end);
-                }
-                return false;
+        
+        for(pair<int,int> x: db){
+            if(start<x.second && end>x.first) return false;
+        }
+        for(pair<int,int> x : b){
+            if(start<x.second && end>x.first){
+                db.push_back({max(start,x.first),min(end,x.second)});
             }
         }
+        b.push_back({start,end});
         return true;
+        
     }
 };
-
-/**
- * Your MyCalendarTwo object will be instantiated and called as such:
- * MyCalendarTwo* obj = new MyCalendarTwo();
- * bool param_1 = obj->book(start,end);
- */
