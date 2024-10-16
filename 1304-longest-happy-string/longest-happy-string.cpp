b464510126c978e53f5153eaaeb3556d13d22dee
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-10-16 14:02:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (97.08%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int,char>> pq;
-        if(a>0) pq.push({a,'a'});
-        if(b>0) pq.push({b,'b'});
-        if(c>0) pq.push({c,'c'});
+        int currA=0,currB=0,currC=0;
         string ans="";
-        while(!pq.empty()){
-            auto x = pq.top();
-            pq.pop();
-            if(ans.length()>=2 and ans[ans.length()-1] == x.second and ans[ans.length()-2]==x.second){
-                if(pq.empty()) break;
-                auto temp = pq.top();
-                pq.pop();
-                ans += temp.second;
-                if(temp.first-1>0){
-                    pq.push({temp.first-1,temp.second});
-                }
+        int temp=a+b+c;
+        for(int i=0;i<temp;i++){
+            if((a>=b && a>=c && currA!=2) or (a>0 and (currB==2 or currC==2))){
+                ans+='a';
+                a--;
+                currA++;
+                currB=0;
+                currC=0;
             }
-            else{
-                x.first--;
-                ans = ans + x.second;
+            else if((b>=a and b>=c and currB!=2) or (b>0 and(currC==2 or currA==2))){
+                ans+='b';
+                b--;
+                currB++;
+                currA=0;
+                currC=0;
             }
-
-            if(x.first>0){
-                pq.push({x.first,x.second});
+            else if((c>=a and c>=b and currC!=2) or (c>0 and (currA==2 or currB==2))){
+                ans+='c';
+                c--;
+                currC++;
+                currB=0;
+                currA=0;
             }
         }
         return ans;
