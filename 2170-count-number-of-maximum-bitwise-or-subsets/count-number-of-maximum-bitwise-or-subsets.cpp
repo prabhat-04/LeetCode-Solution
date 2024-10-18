19531098f64853e2a6de031f9de3d116395df81c
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-10-18 12:57:44 +0530

    Time: 75 ms (26.34%) | Memory: 10.2 MB (31.38%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -7,15 +7,6 @@ int speed = []() {
 
 
 class Solution {
-    int solve(vector<int> &nums,int ind,int curr,int maxi,vector<vector<int>> &memo){
-        if(ind == nums.size()){
-            return (curr==maxi)?1:0;
-        }
-        if(memo[ind][curr]!=-1) return memo[ind][curr];
-        int x = solve(nums,ind+1,curr,maxi,memo);
-        int y = solve(nums,ind+1,curr|nums[ind],maxi,memo);
-        return memo[ind][curr] = x+y;
-    }
 public:
     int countMaxOrSubsets(vector<int>& nums) {
         int n = nums.size();
@@ -23,7 +14,15 @@ public:
         for(auto &it:nums){
             maxi |= it;
         }
-        vector<vector<int>> memo(n,vector<int> (maxi+1,-1));
-        return solve(nums,0,0,maxi,memo);
+        int ans =0,total=1<<n;
+        for(int i=0;i<total;i++){
+            int curr=0;
+            for(int j=0;j<n;j++){
+                if((i>>j & 1)==1)
+                    curr |= nums[j];
+            }
+            if(curr==maxi) ans++;
+        }
+        return ans;
     }
 };
