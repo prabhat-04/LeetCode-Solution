30eaf36e62ddff422b998e5507977ba2b6071fc0
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-10-19 10:18:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (87.54%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.cpp
@@ -1,30 +1,13 @@
-int speed = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    return 0;
-}();
-
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        if(k==1) return '0';
-        if(k==2 || k==3 || k==4) return '1'; 
-        vector<int>vec(k);
-        vec[0]=0;
-        vec[1]=1;
-        vec[2]=1;
-        vec[3]=1;
-        for(int i=4;i<k;i++){
-            int j=i-2;
-            while(j>=0){
-                vec[i] = !vec[j];
-                if(i==k-1) return vec[k-1]+'0';
-                j--;
-                i++;
-            }
-            vec[i]=1;
+        int positionInSection = k & -k;
+        bool isInInvertedPart = ((k / positionInSection) >> 1 & 1) == 1;
+        bool originalBitIsOne = (k & 1) == 0;
+        if (isInInvertedPart) {
+            return originalBitIsOne ? '0' : '1';
+        } else {
+            return originalBitIsOne ? '1' : '0';
         }
-        return vec[k-1]+'0';
     }
 };
