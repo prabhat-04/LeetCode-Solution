995f1633821c20800db6b363987a5a6549d332ed
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-10-21 11:44:08 +0530

    Time: 15 ms (99.42%) | Memory: 12.8 MB (93.86%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,24 +1,33 @@
+int speed = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
+
 class Solution {
 public:
     int maxUniqueSplit(string s) {
         unordered_set<string> seen;
-        return backtrack(s, 0, seen);
+        int maxCount = 0;
+        backtrack(s, 0, seen, 0, maxCount);
+        return maxCount;
     }
 
 private:
-    int backtrack(const string& s, int start, unordered_set<string>& seen) {
-        if (start == s.size()) return 0;
-
-        int maxCount = 0;
-
+    void backtrack(const string& s, int start, unordered_set<string>& seen,int count, int& maxCount) {
+        if (count + (s.size() - start) <= maxCount) return;
+        if (start == s.size()) {
+            maxCount = max(maxCount, count);
+            return;
+        }
         for (int end = start + 1; end <= s.size(); ++end) {
             string substring = s.substr(start, end - start);
             if (seen.find(substring) == seen.end()) {
                 seen.insert(substring);
-                maxCount = max(maxCount, 1 + backtrack(s, end, seen));
+                backtrack(s, end, seen, count + 1, maxCount);
                 seen.erase(substring);
             }
         }
-        return maxCount;
     }
 };
