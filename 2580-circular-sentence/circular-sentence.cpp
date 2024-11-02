76fe52f7f953fca99fcde3d5c0124ec884d32cb9
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-11-02 12:00:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (59.78%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        if(s.size()==0) return true;
+        if(s[0]!=s[s.size()-1]) return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                if(s[i-1]!=s[i+1]) return false;
+            }
+        }
+        return true;
+    }
+};
