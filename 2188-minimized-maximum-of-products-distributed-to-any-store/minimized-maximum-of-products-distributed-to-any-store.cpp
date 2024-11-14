f59331eb24c21dd88165aa9ae41b837060b89606
Prabhat Keshari <prabhatkeshari2004@gmail.com>
2024-11-14 14:34:33 +0530

    Time: 287 ms (10.86%) | Memory: 146.4 MB (9.24%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool feasible(int n,vector<int> quantities,int mid){
+        // cout<<mid<<endl;
+        int i=0,j=0;
+        while(i<n && j<quantities.size()){
+            if(quantities[j]>=mid){
+                quantities[j]-=mid;
+            }
+            else{
+                quantities[j]=0;
+            }
+            if(quantities[j]==0) j++;
+            i++;
+        }
+        // cout<<quantities[quantities.size()-1]<<endl;
+        if(quantities[quantities.size()-1]==0) return true;
+        return false;
+    }
+public:
+    int minimizedMaximum(int n, vector<int>& quantities) {
+        int right = *max_element(quantities.begin(),quantities.end());
+        int left =0;
+        int ans =0;
+        while(left<=right){
+            int mid = left+ (right-left)/2;
+            if(feasible(n,quantities,mid)){
+                ans=mid;
+                right=mid-1;
+            }
+            else
+                left=mid+1;
+        }
+        return ans;
+    }
+};
